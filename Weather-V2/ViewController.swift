251cacc7fcd0d15d6d32c601b5b6e3712bd462fe
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-08-09 11:02:43 +0300

    location setup

--- Weather-V2/ViewController.swift
+++ Weather-V2/ViewController.swift
@@ -10,10 +10,12 @@ import CoreLocation
  
 // custom cell - CollectionView
 // API / request to get the data
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
    
     
     @IBOutlet var table: UITableView!
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
     var models = [Weather]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +26,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         table.delegate = self
         table.dataSource = self
+        
+        setupLocation()
+    }
+    //Location
+    func setupLocation(){
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil{
+            currentLocation = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLocation()
+        }
+    }
     
+    //Table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+    
+    func requestWeatherForLocation(){
+        guard let long = currentLocation?.coordinate.longitude else {return}
+        guard let lat = currentLocation?.coordinate.latitude else {return}
+        
+        print(long, lat)
     }
 
 
